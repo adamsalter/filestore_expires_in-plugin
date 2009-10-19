36e197dac858b7e08124f4dabb30d6bfff47e180
dolzenko <dolzenko@gmail.com>
2009-10-20 05:57:14 +0800

    Make compatible with CacheHelper#cache

--- lib/active_support/cache/file_store_extras.rb
+++ lib/active_support/cache/file_store_extras.rb
@@ -6,7 +6,7 @@ module ActiveSupport
       
       def read(name, options = {})
         lastmod = File.mtime(real_file_path(name)) rescue nil
-        if lastmod && options[:expires_in] && ((lastmod + options[:expires_in]) <= Time.now)
+        if lastmod && options.is_a?(Hash) && options[:expires_in] && ((lastmod + options[:expires_in]) <= Time.now)
           nil
         else  
           original_read(name, options)
